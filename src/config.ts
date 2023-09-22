ea2c8f0632f4dbf8c5f5987debe2e5cdcaca539b
potvik <yuriy@harmony.one>
2023-09-22 22:22:49 +0300

    port fix

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,6 @@ import * as process from 'process';
 export default () => ({
   version: process.env.npm_package_version || '0.0.1',
   name: process.env.npm_package_name || '',
-  port: parseInt(process.env.PORT, 10) || 7860,
+  port: parseInt(process.env.PORT, 10) || 3001,
   SERVERS: process.env.SERVERS
 });
